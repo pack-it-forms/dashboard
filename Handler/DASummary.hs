15c44dee440160b6b1edd72f45d89b1104fb3006
Keith Amidon <camalot@picnicpark.org>
2015-09-27 09:16:48 -0700

    Add basic page structure and navigation
    There are there main kinds of pages:
    
    1. The overall city summary, by zone
    2. The zone summary, by BAT
    3. The BAT details
    
    This commit puts in place the structure to render each page type and a
    navigation bar to switch between them.

--- Handler/DASummary.hs
+++ Handler/DASummary.hs
@@ -1,8 +1,45 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+module Handler.DASummary where
+
 import Import
 import PackItForms.MsgFmt as MF
 import System.FSNotify as FN
 
 watcher d = FN.withManager  $ \mgr -> do
-            FN.watchDir mgr d (const True) printn
+            FN.watchDir mgr d (const True) print
+
+getSummaryR :: Handler Html
+getSummaryR = do
+  let zones = [1..11]
+      bats = []
+  defaultLayout $ do
+    setTitle "Los Altos Damage Status - Summary"
+    let czone = Nothing :: Maybe Integer
+        cbat = Nothing :: Maybe Integer
+        navbar = $(widgetFile "navbar")
+    $(widgetFile "summary")
+
+
+getZoneR :: Integer -> Handler Html
+getZoneR zone = do
+  let zones = [1..11]
+      bats = [118..131]
+  defaultLayout $ do
+    setTitle $ "Los Altos Damage Status - Zone " ++ (toHtml zone)
+    let czone  = Just zone
+        cbat = Nothing :: Maybe Integer
+        navbar = $(widgetFile "navbar")
+    $(widgetFile "zone")
+
+getBatR :: Integer -> Integer -> Handler Html
+getBatR zone bat = do
+  let zones = [1..11]
+      bats = [118..131]
+  defaultLayout $ do
+    setTitle $ "Los Altos Damage Status - BAT " ++ (toHtml bat)
+    let czone = Just zone
+        cbat = Just bat
+        navbar = $(widgetFile "navbar")
+    $(widgetFile "bat")
+
