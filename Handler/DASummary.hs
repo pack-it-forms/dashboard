2c7d0510b2e88b2ac9d5dc4e1a74129fc19bbdd9
Keith Amidon <camalot@picnicpark.org>
2015-10-08 18:22:05 -0700

    Generalize map widget to show zone/BAT maps too
    This commit is a substantial improvement in the map widget
    functionality.  It:
    
    - Adds the ability to clip the map based on one or more polygon features
      in a KML file.
    
    - Zooms the map to so that the clipped area fills as much of the area
      allocated for the map as possible.
    
    - Adds the ability to overlay polygons from another KML file over the
      map.
    
    - Adds the ability to change the colors of polygons in the overlay layer
    
    - Uses all these features to display maps at the city, zone, and BAT
      levels with overlaid areas.

--- Handler/DASummary.hs
+++ Handler/DASummary.hs
@@ -3,12 +3,11 @@
 module Handler.DASummary where
 
 import Import
-import PackItForms.MsgFmt as MF
-import System.FSNotify as FN
 import qualified PackItForms.LADamage as LAD
+-- import System.FSNotify as FN
 
-watcher d = FN.withManager  $ \mgr -> do
-            FN.watchDir mgr d (const True) print
+-- watcher d = FN.withManager  $ \mgr -> do
+--             FN.watchDir mgr d (const True) print
 
 getSummaryR :: Handler Html
 getSummaryR = do
@@ -20,7 +19,9 @@ getSummaryR = do
     let czone = Nothing :: Maybe Integer
         cbat = Nothing :: Maybe Integer
         navbar = $(widgetFile "navbar")
-        map = mapW "zoneMap" (-122.09847, 37.36513, 13)
+        areamap = mapW "cityMap" (-122.09847, 37.36513, 13)
+                    ("/static/map-data/zones.kml", [])
+                    ("/static/map-data/zones.kml", [])
     $(widgetFile "summary")
 
 
@@ -34,6 +35,9 @@ getZoneR zone = do
     let czone  = Just zone
         cbat = Nothing :: Maybe Integer
         navbar = $(widgetFile "navbar")
+        areamap = mapW "zoneMap" (-122.09847, 37.36513, 13)
+                    ("/static/map-data/zones.kml", ["Zone " ++ (show zone)])
+                    ("/static/map-data/bats.kml", ["BAT " ++ (show b) | b <- bats])
     $(widgetFile "zone")
 
 getBatR :: Integer -> Integer -> Handler Html
@@ -46,14 +50,19 @@ getBatR zone bat = do
     let czone = Just zone
         cbat = Just bat
         navbar = $(widgetFile "navbar")
+        areamap = mapW "batMap" (-122.09847, 37.36513, 13)
+                    ("/static/map-data/bats.kml", ["BAT " ++ (show bat)])
+                    ("/static/map-data/bats.kml", ["BAT " ++ (show bat)])
     $(widgetFile "bat")
 
 type Longitude = Double
 type Lattitude = Double
 type MapZoom = Integer
 
-mapW :: String -> (Longitude, Lattitude, MapZoom) -> Widget
-mapW mapId (longitude, lattitude, zoom) = do
-  addStylesheetRemote "http://openlayers.org/en/v3.9.0/css/ol.css"
-  addScriptRemote "http://openlayers.org/en/v3.9.0/build/ol.js"
+mapW :: String -> (Longitude, Lattitude, MapZoom) -> (String, [String]) -> (String, [String]) -> Widget
+mapW mapId (longitude, lattitude, zoom)
+           (clip_source_kml, clip_source_features)
+           (areas_source_kml, areas_source_features) = do
+  addStylesheet $ StaticR openlayers_ol_css
+  addScript $ StaticR openlayers_ol_js
   $(widgetFile "map")
