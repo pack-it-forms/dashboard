e9286a8ee9c56bdb4a91bbf8ea4fbe85c01ad307
Keith Amidon <keith@awakenetworks.com>
2018-01-19 07:27:15 -0800

    Pin nixpkgs and pack-it-forms-msgfmt for release builds
    nixpkgs is pinned to the tip of the 17.09 branch at the time of this
    commit. The intention is to keep it on the most recent stable commit
    at each release.
    
    pack-it-forms-msgfmt is pinned to the tip of master at the time of
    this commit. It is expected that it will co-evolve with the dashboard
    and is likely to be updated fairly frequently.

--- project-deps.nix
+++ project-deps.nix
@@ -4,8 +4,8 @@ self: super:
   nixpkgs = self.releaseGHDevLocal {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "TODO";
-    sha256 = "TODO";
+    rev = "66b4de79e3841530e6d9c6baf98702aa1f7124e4";
+    sha256 = "1l3lwi944hnxka0nfq9a1g86xhc0b8hzqr2fm6cvds33gj26l0g4";
     local = <nixpkgs>;
     fetch = super.nixpkgs.fetchFromGitHub;
   } { config = {}; };
@@ -14,8 +14,8 @@ self: super:
     pack-it-forms-msgfmt = self.releaseGHDevLocal {
       owner = "pack-it-forms";
       repo = "msgfmt";
-      rev = "TODO";
-      sha256 = "TODO";
+      rev = "64274e480c88921f418f96fa8c581c286d173642";
+      sha256 = "1hig48wgbams379hwm7q9hjygilm1gmjqaq1i7llfw1sk9wps7sy";
       local = ../pack-it-forms-msgfmt;
     };
   };
